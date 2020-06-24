ae058626000762fe072335ca1c44f57d05e94339
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-06-24 20:02:25 +0300

    define everything in impact.feature.greenworks.greenworks

--- modules/impact.feature.greenworks.greenworks.d.ts
+++ modules/impact.feature.greenworks.greenworks.d.ts
@@ -1,2 +1,28 @@
 // requires impact.base.game.d.ts
 // requires impact.base.vars.d.ts
+
+declare namespace ig {
+  namespace Greenworks {
+    type Step = 'loaded' | 'initialized' | 'activated' | 'cleared';
+  }
+  interface Greenworks extends ig.GameAddon {
+    // TODO: TS defs for greenworks, see https://github.com/greenheartgames/greenworks/tree/master/docs
+    // this is absolutely not critical though, maybe even useless
+    greenworks: any;
+    steps: Greenworks.Step[];
+
+    isActive(this: this): boolean;
+    hasSteamStartArgument(this: this): boolean;
+    activateAchievement(
+      this: this,
+      achievement: string,
+      clearAfterwards: boolean, // ???
+    ): void;
+    clearAchievement(this: this, achievement: string): void;
+  }
+  interface GreenworksConstructor extends ImpactClass<Greenworks> {
+    new (): Greenworks;
+  }
+  var Greenworks: GreenworksConstructor;
+  var greenworks: Greenworks;
+}
