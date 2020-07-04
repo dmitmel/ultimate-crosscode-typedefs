ebb08915e4212b7b8a0c37c1632894bd8bb6f15a
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-07-05 02:26:20 +0300

    crosscode-ru development

--- modules/game.feature.menu.gui.lore.lore-misc.d.ts
+++ modules/game.feature.menu.gui.lore.lore-misc.d.ts
@@ -6,10 +6,16 @@ declare namespace sc {
   interface LoreInfoBox extends ig.BoxGui {
     title: sc.TextGui;
     category: sc.TextGui;
+    alternativeArrow: ig.ImageGui;
+    alternative: sc.TextGui;
+    scrollContainer: sc.ScrollPane;
+    focus: boolean;
 
     setLore(this: this, key: string): void;
   }
-  interface LoreInfoBoxConstructor extends ImpactClass<LoreInfoBox> {}
+  interface LoreInfoBoxConstructor extends ImpactClass<LoreInfoBox> {
+    new (): LoreInfoBox;
+  }
   var LoreInfoBox: LoreInfoBoxConstructor;
 
   interface LoreEntryButton extends sc.ListBoxButton {}
