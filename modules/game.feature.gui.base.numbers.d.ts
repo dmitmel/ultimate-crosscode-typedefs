69a9d5e21426785b4f64092ffecd94d8057cda05
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2020-12-07 13:44:30 +0200

    crosscode-ru development

--- modules/game.feature.gui.base.numbers.d.ts
+++ modules/game.feature.gui.base.numbers.d.ts
@@ -4,7 +4,9 @@ export {};
 
 declare global {
   namespace sc {
-    interface NumberGui extends ig.GuiElementBase {}
+    interface NumberGui extends ig.GuiElementBase {
+      setNumber(this: this, number: number, skipTransition?: boolean | null): void;
+    }
     interface NumberGuiConstructor extends ImpactClass<NumberGui> {}
     var NumberGui: NumberGuiConstructor;
   }
--- modules/game.feature.menu.gui.shop.shop-confirm.d.ts
+++ modules/game.feature.menu.gui.shop.shop-confirm.d.ts
@@ -5,3 +5,20 @@ import './impact.feature.gui.base.box';
 import './game.feature.menu.gui.menu-misc';
 import './game.feature.gui.widget.modal-dialog';
 import './game.feature.menu.gui.shop.shop-misc';
+
+export {};
+
+declare global {
+  namespace sc {
+    interface ShopConfirmEntry extends ig.GuiElementBase {
+      gfx: ig.Image;
+      item: sc.TextGui;
+      amount: sc.NumberGui;
+      price: sc.NumberGui;
+    }
+    interface ShopConfirmEntryConstructor extends ImpactClass<ShopConfirmEntry> {
+      new (name: string, amount: number, price: number): ShopConfirmEntry;
+    }
+    var ShopConfirmEntry: ShopConfirmEntryConstructor;
+  }
+}
