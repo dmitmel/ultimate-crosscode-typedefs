5850ec2d6232e33e9a0263be18cf953cb88c7d28
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2021-02-20 00:21:03 +0200

    crosscode-ru development

--- modules/game.feature.menu.menu-model.d.ts
+++ modules/game.feature.menu.menu-model.d.ts
@@ -32,15 +32,116 @@ export {};
 
 declare global {
   namespace sc {
-    enum MENU_QUEST_HUB_TABS {
-      OPEN,
-      ACTIVE,
-      FINISHED,
+    enum MENU_SUBMENU {
+      START = 0,
+      ITEMS = 1,
+      SKILLS = 2,
+      EQUIPMENT = 3,
+      STATUS = 4,
+      SYNOPSIS = 5,
+      MAP = 6,
+      SAVE = 7,
+      OPTIONS = 8,
+      SHOP = 9,
+      QUESTS = 10,
+      TROPHY = 11,
+      LORE = 12,
+      ENEMY = 13,
+      SOCIAL = 14,
+      STATS = 15,
+      MUSEUM = 16,
+      TRADE = 17,
+      BOTANICS = 18,
+      QUEST_HUB = 19,
+      ARENA = 20,
+      NEW_GAME = 21,
     }
 
+    enum MENU_LEA_STATE {
+      LARGE = 0,
+      SMALL = 1,
+      HIDDEN = 2,
+    }
+
+    enum MENU_EQUIP_BODYPART {
+      NONE = 0,
+      HEAD = 1,
+      RIGHT_ARM = 2,
+      LEFT_ARM = 3,
+      TORSO = 4,
+      FEET = 5,
+    }
+
+    enum MENU_EQUIP_BODYPART_STR {
+      HEAD = 'head',
+      LEFT_ARM = 'leftArm',
+      RIGHT_ARM = 'rightArm',
+      TORSO = 'torso',
+      FEET = 'feet',
+    }
+
+    enum MENU_SHOP_TYPES {
+      BUY_AND_SELL = 0,
+      BUY = 1,
+      SELL = 2,
+      COIN = 3,
+    }
+
+    enum MENU_SHOP_STATE {
+      START = 0,
+      BUY = 1,
+      SELL_CAT = 2,
+      SELL = 3,
+    }
+
+    enum LOG_TYPES {
+      QUEST = 0,
+      LANDMARK = 1,
+      LORE = 2,
+      TROPHY = 3,
+      TRADER = 4,
+      STORY = 5,
+      DROP = 6,
+    }
+
+    enum MENU_STATUS_PAGES {
+      MAIN = 0,
+      PARAMS = 1,
+      MODS = 2,
+      COMBAT_ARTS = 3,
+    }
+
+    enum BOTANICS_SORT_TYPE {
+      ORDER = 0,
+      FOUND = 1,
+      NAME = 2,
+    }
+
+    var MENU_STATUS_PAGES_LENGTH: number;
+
+    var SELL_PAGES: Array<{
+      lang: string;
+      type: sc.ITEMS_TYPES;
+      equipType: sc.ITEMS_EQUIP_TYPES;
+    }>;
+
+    enum MENU_QUEST_HUB_TABS {
+      OPEN = 0,
+      ACTIVE = 1,
+      FINISHED = 2,
+    }
+
+    enum MENU_ARENA_TABS {
+      SOLO = 0,
+      TEAM = 1,
+    }
+
+    var MAX_STAMPS_MAX: number;
+    var MAX_MOD_VAL: number;
+
     enum TOGGLE_SET_TYPE {
-      SINGLE,
-      MULTI,
+      SINGLE = 'SINGLE',
+      MULTI = 'MULTI',
     }
 
     interface MenuModel extends ig.GameAddon, sc.Model {
