3020c3408b3f70516c3f1185dd4d8d592588926a
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2022-02-01 01:04:57 +0200

    crosscode-ru development

--- modules/impact.feature.gui.base.box.d.ts
+++ modules/impact.feature.gui.base.box.d.ts
@@ -4,6 +4,10 @@ export {};
 
 declare global {
   namespace ig {
+    interface NinePatch extends ig.Class {}
+    interface NinePatchConstructor extends ImpactClass<NinePatch> {}
+    var NinePatch: NinePatchConstructor;
+
     interface BoxGui extends ig.GuiElementBase {}
     interface BoxGuiConstructor extends ImpactClass<BoxGui> {}
     var BoxGui: BoxGuiConstructor;
