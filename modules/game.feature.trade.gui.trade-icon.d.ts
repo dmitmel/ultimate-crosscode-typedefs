bb8f82d23b7192257806d494e2ea5c1348c12c8f
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2022-02-15 00:59:13 +0200

    crosscode-es development

--- modules/game.feature.trade.gui.trade-icon.d.ts
+++ modules/game.feature.trade.gui.trade-icon.d.ts
@@ -8,14 +8,26 @@ export {};
 declare global {
   namespace sc {
     interface TradeIconGui extends ig.BoxGui {
+      numberGfx: ig.Image;
+      tradeInfo: any; // TODO
+      tradeIcon: unknown;
+      lineGui: ig.ColorGui;
       entries: Array<{
         require: any; // TODO
         gui: sc.TextGui & { tradeName: string } & sc.TextGui.LevelDrawData;
       }>;
 
+      setIconState(this: this, state: sc.INTERACT_ENTRY_STATE): void;
+      isActive(this: this, state: sc.INTERACT_ENTRY_STATE): boolean;
       _createContent(this: this): void;
+      _createStatic(this: this): void;
+      _updateTexts(this: this): void;
+      _hasMissingItem(this: this, require: any): boolean | undefined;
+      remove(this: this): void;
     }
-    interface TradeIconGuiConstructor extends ImpactClass<TradeIconGui> {}
+    interface TradeIconGuiConstructor extends ImpactClass<TradeIconGui> {
+      new (trader: string): TradeIconGui;
+    }
     var TradeIconGui: TradeIconGuiConstructor;
   }
 }
