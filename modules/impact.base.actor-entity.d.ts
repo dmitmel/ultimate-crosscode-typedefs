cbb62c92d9a9c03bafee4e47c78f1af0a8beb50d
EL2020 <el2020202020@gmail.com>
2022-12-22 16:44:01 -0500

    combat conditions

--- modules/impact.base.actor-entity.d.ts
+++ modules/impact.base.actor-entity.d.ts
@@ -5,9 +5,32 @@ export {};
 
 declare global {
   namespace ig {
+    namespace ActorEntity {
+      interface WalkAnims {
+        idle: string;
+        preMove?: string;
+        move?: string;
+        moveRev?: string;
+        moveLeft?: string;
+        run?: string;
+        runRev?: string;
+        runLeft?: string;
+        brake?: string;
+        preIdle?: string;
+        jump?: string;
+        fall?: string;
+        hover?: string;
+        preHoverMove?: string;
+        hoverMove?: string;
+        hoverMoveRev?: string;
+        land?: string;
+      }
+    }
+
     interface ActorEntity extends ig.AnimatedEntity {
       face: Vec2;
       animationFixed: boolean;
+      walkAnims: ActorEntity.WalkAnims;
 
       setAction(
         this: this,
@@ -18,6 +41,8 @@ declare global {
       getTarget(this: this): ig.ActorEntity | null | undefined;
       setAttribute(this: this, key: string, value: unknown): void;
       getAttribute(this: this, key: string): unknown;
+      storeWalkAnims(this: this, name: string, config: ig.ActorEntity.WalkAnims): void;
+      setWalkAnims(this: this, config: string | ig.ActorEntity.WalkAnims): void;
     }
     interface ActorEntityConstructor extends ImpactClass<ActorEntity> {}
     var ActorEntity: ActorEntityConstructor;
